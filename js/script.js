7dae2d74b84f0846750b09dd2f6e33a382d1ef68
adaamll <adam.l@live.se>
2023-02-24 06:22:54 +0100

    flexbox fix

--- js/script.js
+++ js/script.js
@@ -5,7 +5,11 @@ function mobileNav() {
 
   btnNavEl.addEventListener('click', () => headerEl.classList.toggle('nav-open'));
 }
-mobileNav();
+
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+
+btnNavEl.addEventListener('click', () => headerEl.classList.toggle('nav-open'));
 
 // Sticky navigation
 function stickyNav() {
@@ -66,3 +70,23 @@ function setCurrentYear() {
   yearEl.textContent = currentYear;
 }
 setCurrentYear();
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  const flex = document.createElement('div');
+  flex.style.display = 'flex';
+  flex.style.flexDirection = 'column';
+  flex.style.rowGap = '1px';
+
+  flex.appendChild(document.createElement('div'));
+  flex.appendChild(document.createElement('div'));
+
+  document.body.appendChild(flex);
+  const isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add('no-flexbox-gap');
+}
+checkFlexGap();
